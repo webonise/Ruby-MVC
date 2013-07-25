04e7528691fa753549705c7d9a66578a3f6a78d9
AdityaKamatar <aditya.kamatar@weboniselab.com>
2013-07-25 14:13:39 +0530

    Routing Changes

--- webonise/app/controllers/bikers_controller.rb
+++ webonise/app/controllers/bikers_controller.rb
@@ -1,5 +1,11 @@
 class BikersController < Surfer::Controller
 def show
-"This is a message from BikersController for you webonisers -- This will be awesome !! "
+	#render :show
+	"This is a message from BikersController for you webonisers -- This will be awesome !! "
 end
+
+def index
+	"Inside Index"
+end
+
 end
