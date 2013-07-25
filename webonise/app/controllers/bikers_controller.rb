490e0110dc68dbc6dd4f39bd2bc438037f8fb60a
AdityaKamatar <aditya.kamatar@weboniselab.com>
2013-07-25 14:26:39 +0530

    Revert "Routing Changes"
    This reverts commit 04e7528691fa753549705c7d9a66578a3f6a78d9.

--- webonise/app/controllers/bikers_controller.rb
+++ webonise/app/controllers/bikers_controller.rb
@@ -1,11 +1,5 @@
 class BikersController < Surfer::Controller
 def show
-	#render :show
-	"This is a message from BikersController for you webonisers -- This will be awesome !! "
+"This is a message from BikersController for you webonisers -- This will be awesome !! "
 end
-
-def index
-	"Inside Index"
-end
-
 end
